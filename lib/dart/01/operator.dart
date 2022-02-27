e20ad3dbe4151937b5b18a035a19d1157d06358e
tjdans345 <tjdans34568@gmail.com>
2022-02-27 21:19:05 +0900

    operator

--- lib/dart/01/operator.dart
+++ lib/dart/01/operator.dart
@@ -3,5 +3,32 @@ void main() {
 
   int number = 2;
 
+  // %는 나머지를 반환한다.
+  print(number % 2);
+
+  number++;
+  print(number);
+  print(number % 2);
+
+  number--;
+  print(number);
+
+  double number2 = 4.0;
+  print(number2);
+  number2 += 3;
+  print(number2);
+  number2 *= 3;
+  print(number2);
+
+  print("----------------------------------------");
+  // null 조건 operator
+  double? number3 = 4.0;
+  print(number3);
+
+  number3 = null;
+  number3 ??= 3.0; // ?? => null 이면 값을 대입해라는 뜻
+  print(number3);
+
+
 
 }
