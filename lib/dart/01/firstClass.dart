0a6e80b3c546e629a122fd8e08bba915fcd1ccd9
tjdans345 <tjdans34568@gmail.com>
2022-02-27 20:43:53 +0900

    integer

--- lib/dart/01/firstClass.dart
+++ lib/dart/01/firstClass.dart
@@ -2,30 +2,31 @@
 void main() {
 
   // variable
+  // var 를 이용해 선언한 변수는 같은 스코프안에서는 재선언은 불가능하다 (JS의 let이랑 비슷하네)
   var name = "메테오";
-
   print(name);
 
   var name2 = "레드벨벳";
-
   print(name2);
 
   name = "플러터";
-
   print(name);
 
-  // var 를 이용해 선언한 변수는 같은 스코프안에서는 재선언은 불가능하다 (JS의 let이랑 비슷하네)
+
 
   // 변수 타입
 
   // 01. 정수
   // integer
   int number = 10;
+  print(number);
 
-
-
-
-
+  int number1 = 2;
+  int number2 =4;
+  print(number1 + number2);
+  print(number1 - number2);
+  print(number1 / number2);
+  print(number1 * number2);
 
 
 
