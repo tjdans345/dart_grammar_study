757843c3e76dc6b15354d1dc0a06d081f75b5ff7
tjdans345 <tjdans34568@gmail.com>
2022-02-27 20:42:11 +0900

    Initial commit

---
+++ android/app/src/main/java/com/example/dart_grammar_study/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.dart_grammar_study;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/dart/01/firstClass.dart
@@ -0,0 +1,42 @@
+
+void main() {
+
+  // variable
+  var name = "메테오";
+
+  print(name);
+
+  var name2 = "레드벨벳";
+
+  print(name2);
+
+  name = "플러터";
+
+  print(name);
+
+  // var 를 이용해 선언한 변수는 같은 스코프안에서는 재선언은 불가능하다 (JS의 let이랑 비슷하네)
+
+  // 변수 타입
+
+  // 01. 정수
+  // integer
+  int number = 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
