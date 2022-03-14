9f7a7152b698d6352e35ba4263b6eb4576ec02b3
tjdans34568 <tjdans34568@gmail.com>
2022-03-14 21:09:14 +0900

    stream update

--- lib/dart/04. 비동기 프로그래밍/stream2.dart
+++ lib/dart/04. 비동기 프로그래밍/stream2.dart
@@ -15,13 +15,12 @@ void main() {
 
 }
 
-// Fucture 의 await 처럼 쓰기 위한 방법
+// Future 의 await 처럼 쓰기 위한 방법
 Stream<int> playAllStream() async* {
   // yield* 을 사용하면 해당 스트림의 값을 다 들고 올 때 까지 기다린다.
   yield* calculate(1);
   yield* calculate(1000);
 }
-
 
 // 함수로 스트리밍 사용하기;
 // Stream 은 async 뒤에 * 을 붙여준다.
