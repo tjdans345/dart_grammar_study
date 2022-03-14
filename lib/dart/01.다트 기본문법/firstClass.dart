77d74bed1630beb96dd1cec10aa4077388118320
tjdans34568 <tjdans34568@gmail.com>
2022-03-14 22:08:50 +0900

    firstClass modify

--- lib/dart/01.다트 기본문법/firstClass.dart
+++ lib/dart/01.다트 기본문법/firstClass.dart
@@ -40,7 +40,7 @@ void main() {
 
   // 04. 문자열 타입
   String girlGroup = "레드벨벳쨔응";
-  print(girGroup);
+  print(girlGroup);
 
   // 05. var : 타입 유추 -> 런타임시 타입 정해짐
   // var 로 다 선언되있으면 무슨 타입인지 알아보기 힘들다
